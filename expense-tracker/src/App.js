b1e30366dc5b8e844dfc95fcb3dcc1908e9838c6
senker <vla.rotaru@gmail.com>
2023-02-09 21:59:16 +0100

    feat: adding form inputs for create new expense

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components//Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expenses = [
     {
@@ -30,6 +30,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
