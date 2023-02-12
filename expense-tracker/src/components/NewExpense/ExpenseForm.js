4bcf1a390ea83ecb29c69d4ae8a154c5a8d07e0c
senker <vla.rotaru@gmail.com>
2023-02-12 20:31:21 +0100

    feat: Child-To-Parent component communication

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   // Individual state
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
@@ -53,7 +53,8 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    /* console.log(expenseData); */
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
--- expense-tracker/src/components/NewExpense/NewExpense.js
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -3,9 +3,19 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import "./NewExpense.css"
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            // TODO: generate not a random string   
+            id: Math.random().toString()
+        };
+        /* console.log(expenseData); */
+        props.onAddExpense(expenseData);
+    };
+
     return <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
 };
 
