8f99b655dfb422e8c565ad2ae2339bbcab8b5cc5
senker <vla.rotaru@gmail.com>
2023-03-22 16:20:59 +0100

    ❇️ feat: use CSS modules

--- styling-practice-project/src/components/UI/Button/Button.js
+++ styling-practice-project/src/components/UI/Button/Button.js
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import React from 'react';
+
+import styles from './Button.module.css'
+/* import styled from "styled-components";
 
 const Button = styled.button`
   width: 100%;
@@ -24,14 +27,14 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`;
+`; */
 
-/* const Button = props => {
+const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
- */
+
 export default Button;
