08d336067f05b42a711955f278bc3a9facc21839
senker <vla.rotaru@gmail.com>
2023-03-22 15:58:27 +0100

    ❇️ feat: styled components & dynamic props

--- styling-practice-project/src/components/UI/Button/Button.js
+++ styling-practice-project/src/components/UI/Button/Button.js
@@ -9,12 +9,12 @@ const Button = styled.button`
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
 
-  &:focus {
+  & focus {
     outline: none;
   }
 
-  &:hover,
-  &:active {
+  & hover,
+  & active {
     background: #ac0e77;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
