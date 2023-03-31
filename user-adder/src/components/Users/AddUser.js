0f349f0d26eb0a77b2754fbad9e2fd390006a0e1
senker <senkerplov@gmail.com>
2023-03-31 16:30:12 +0200

    ❇️ feat: manage user input state

--- user-adder/src/components/Users/AddUser.js
+++ user-adder/src/components/Users/AddUser.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 
 import Card from "../UI/Card";
-import style from './AddUser.module.css'
+import style from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
 
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
   return (
     <Card className={style.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
