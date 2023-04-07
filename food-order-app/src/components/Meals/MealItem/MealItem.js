e9e4a63d1cbb4714898839ae47dddc757079f580
senker <senkerplov@gmail.com>
2023-04-07 14:25:35 +0200

    🚧 refactor: indentation fix

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -4,17 +4,16 @@ import MealItemForm from "./MealItemForm";
 import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
-
   const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price
-    })
+      price: props.price,
+    });
   };
 
   return (
@@ -25,7 +24,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm onAddToCart={addToCartHandler} id={props.id}/>
+        <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
       </div>
     </li>
   );
--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
@@ -8,26 +8,28 @@ const MealItemForm = (props) => {
 
   const amountInputRef = useRef();
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
 
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
 
-    if (enteredAmount.trim().length === 0 
-    || enteredAmountNumber < 1 
-    || enteredAmountNumber > 5) {
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
       setAmountIsValid(false);
       return;
     }
 
     props.onAddToCart(enteredAmountNumber);
-  }
+  };
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
-      ref={amountInputRef}
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
