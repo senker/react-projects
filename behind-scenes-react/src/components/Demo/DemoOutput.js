4fe061285cf9c53165240f666d85708c0376e047
senker <senkerplov@gmail.com>
2023-04-14 21:27:53 +0200

    📚 learning: prevent unnecessary eval with memo()

--- behind-scenes-react/src/components/Demo/DemoOutput.js
+++ behind-scenes-react/src/components/Demo/DemoOutput.js
@@ -6,4 +6,6 @@ const DemoOutput = (props) => {
    return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 }
 
-export default DemoOutput;
+// (React.memo) only if the value of a prop changed, 
+    //than the component should be re-executed and re-evaluated
+export default React.memo(DemoOutput);
