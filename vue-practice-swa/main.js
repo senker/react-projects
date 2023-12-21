ea4e84159dc86d27f1d6f8ad6e1a3ad19cb94614
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-12-21 12:23:40 +0100

    chore: lesson ground for list rendering

--- vue-practice-swa/main.js
+++ vue-practice-swa/main.js
@@ -3,10 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            // solution
-            onSale: true
-            // solution
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
